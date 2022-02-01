e5c3a4e63004d5643c644cc01831392f652c3d03
dzenbots <vv.khritkin@gmail.com>
2022-02-01 23:00:17 +0300

    small fixes

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from aiogram import executor
 
 from loader import dp
-import middlewares, filters, handlers
+# import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify, on_shutdown_notify
 from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
+    import middlewares
+    import filters
+    import handlers
     # Устанавливаем дефолтные команды
     await set_default_commands(dispatcher)
 
     # Уведомляет про запуск
-    await on_shutdown_notify(dispatcher)
+    await on_startup_notify(dispatcher)
 
 
 async def on_shutdown(dispatcher):
-    await on_startup_notify(dispatcher)
+    await on_shutdown_notify(dispatcher)
 
 
 if __name__ == '__main__':
